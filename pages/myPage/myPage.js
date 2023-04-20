35a3cd8d400a7469d3e3a7f70c08c6ad8737841c
devcdd <developer.cdd@gmail.com>
2023-04-20 16:33:35 +0900

    FEAT: myPage Layout & simple Function ADD

---
+++ pages/myPage/myPage.js
@@ -0,0 +1,15 @@
+const userInfo = document.querySelector(".userInfo");
+const userCart = document.querySelector(".userCart");
+const contact = document.querySelector(".contact");
+
+userInfo.addEventListener("click", () => {
+  alert("개인정보 변경");
+});
+
+userCart.addEventListener("click", () => {
+  alert("개인정보 변경");
+});
+
+contact.addEventListener("click", () => {
+  alert("고객센터");
+});
