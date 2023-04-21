356588489c022dca7009be3d4d4d9e9a890a687d
devcdd <developer.cdd@gmail.com>
2023-04-21 17:47:10 +0900

    FEAT: Cart Function Working (local Storage)

--- pages/myPage/myPage.js
+++ pages/myPage/myPage.js
@@ -3,42 +3,69 @@ const userCart = document.querySelector(".userCart");
 const contact = document.querySelector(".contact");
 const userInfoForm = document.querySelector(".userInfoForm");
 const contactForm = document.querySelector("#contact");
+const userCartForm = document.querySelector(".userCartForm");
 
+const saveBtn = document.querySelector(".saveBtn");
+
+// value 용 input 값
+// 사용법: userNameInput.value = ...;
 const userNameInput = document.querySelector("#userName");
 const userEmailInput = document.querySelector("#userEmail");
 const userPasswordInput = document.querySelector("#userPassword");
-const userPhoneInput = document.querySelector("#userPhone");
 const userAddressInput = document.querySelector("#userAddress");
+const userPhoneInput = document.querySelector("#userPhoneInput");
 
-// user 정보 (받아오는 값)
+// user 정보 (데이터베이스로(백엔드)부터 받아오는 값)
+
 const userName = "김종운";
 const userEmail = "cdd@kakao.com";
 const userPassword = "eliceFighting";
-const userPhone = 010;
+const userPhone = "010-1234-1234";
 const userAddress = "부산시 광안대교";
 
 userInfo.addEventListener("click", (e) => {
   e.preventDefault();
-  if (userInfoForm.style.display === "none") {
-    contactForm.style.display = "none";
-    userInfoForm.style.display = "flex";
-  } else userInfoForm.style.display = "none";
+
+  contactForm.style.display = "none";
+  userCartForm.style.display = "none";
+  userInfoForm.style.display = "block";
 
   userNameInput.value = userName;
   userEmailInput.value = userEmail;
   userPasswordInput.value = userPassword;
-  userPhoneInput.value = userPhone;
   userAddressInput.value = userAddress;
+  userPhoneInput.value = userPhone;
 });
 
-userCart.addEventListener("click", () => {
-  alert("개인정보 변경");
+userCart.addEventListener("click", (e) => {
+  e.preventDefault();
+  userInfoForm.style.display = "none";
+  userCartForm.style.display = "block";
+  contactForm.style.display = "none";
 });
 
 contact.addEventListener("click", (e) => {
   e.preventDefault();
-  if (contactForm.style.display === "none") {
-    contactForm.style.display = "flex";
-    userInfoForm.style.display = "none";
-  } else contactForm.style.display = "none";
+
+  userInfoForm.style.display = "none";
+  userCartForm.style.display = "none";
+  contactForm.style.display = "flex";
+});
+
+userPhoneInput.addEventListener("input", function () {
+  const value = userPhoneInput.value.replace(/\D/g, ""); // 입력된 숫자 이외의 문자 모두 제거
+  const regex = /^([0-9]{2,3})([0-9]{3,4})([0-9]{4})$/;
+  const groups = regex.exec(value);
+
+  if (groups) {
+    const tel = groups[1] + "-" + groups[2] + "-" + groups[3];
+    userPhoneInput.value = tel;
+  } else {
+    userPhoneInput.value = value;
+  }
+});
+
+saveBtn.addEventListener("click", () => {
+  alert("저장이 완료되었습니다!");
+  // DB 저장 관련 메서드, 함수
 });
--- pages/productCart/productCart.js
+++ pages/productCart/productCart.js
@@ -1,34 +1,14 @@
-// 쿼리 스트링 파싱
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
+const productCartString = localStorage.getItem("product");
+const productObj = JSON.parse(productCartString);
 
-// 선택된 상품 정보 가져오기
-const productImage = urlParams.get("image");
-const productName = urlParams.get("name");
-const productPrice = urlParams.get("price");
-const productQuantity = urlParams.get("quantity");
+const tbody = document.querySelector("tbody");
 
-const tableBody = document.querySelector("tbody");
-const productRow = document.createElement("tr");
-const productImageCell = document.createElement("td");
+const eachrow = `<tr>
+<th scope="row">1</th>
+<td>${productObj.productImg}</td>
+<td>${productObj.productName}</td>
+<td>${productObj.productQuantity}</td>
+<td>${productObj.productPrice}</td>
+</tr>`;
 
-productImage.src = productImage;
-productImageCell.appendChild(productImage);
-productImageCell.appendChild(document.createTextNode(productName));
-
-const productQuantityCell = document.createElement("td");
-productQuantityCell.textContent = productQuantity;
-
-const productPriceCell = document.createElement("td");
-productPriceCell.textContent = productPrice;
-
-const productTotalCell = document.createElement("td");
-productTotalCell.textContent =
-  parseInt(productPrice) * parseInt(productQuantity);
-
-productRow.appendChild(productImageCell);
-productRow.appendChild(productQuantityCell);
-productRow.appendChild(productPriceCell);
-productRow.appendChild(productTotalCell);
-
-tableBody.appendChild(productRow);
+tbody.innerHTML += eachrow;
